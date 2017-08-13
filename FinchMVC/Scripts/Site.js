cccc80f9b28081bbf2056818e0624bbcafd80a66
Austin Finch <Austin Finch>
2017-08-13 11:51:32 -0500

    Add project files.

---
+++ FinchMVC/Scripts/Site.js
@@ -0,0 +1,16 @@
+﻿$('.ui.left.sidebar').first()
+    .sidebar('setting', 'transition', 'uncover')
+    .sidebar('attach events', '.left-sidebar-toggle')
+    ;
+$('.ui.top.sidebar').first()
+    .sidebar('setting', 'transition', 'overlay')
+    .sidebar('attach events', '.top-sidebar-toggle')
+    ;
+$('.ui.right.sidebar').first()
+    .sidebar('setting', 'transition', 'uncover')
+    .sidebar('attach events', '.right-sidebar-toggle')
+    ;
+
+$('.ui.dropdown').dropdown();
+
+$('.ui.sticky').sticky();
